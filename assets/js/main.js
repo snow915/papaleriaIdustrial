c504698b431760f5e0ede8ba86030d6cac87a169
snow915 <snow915@protonmail.com>
2024-12-09 15:05:57 -0600

    change: font family and remove unnecessary items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Reveal
-* Template URL: https://bootstrapmade.com/reveal-bootstrap-corporate-template/
-* Updated: Aug 07 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 
 (function() {
   "use strict";
